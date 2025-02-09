cd99c99d5cf3d2c2d9c72464610b81dd26261db9
visstt <egorskomorohov020606@gmail.com>
2025-02-09 21:45:44 +0500

    test fetch

--- src/pages/Home/HomeLayaot.jsx
+++ src/pages/Home/HomeLayaot.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./HomeLayaot.module.css";
 import HomeTitle from "./HomeTitle/HomeTitle";
 import HomeDescription from "./HomeDescription/HomeDescription";
@@ -7,10 +7,39 @@ import HomeTeachers from "./HomeTeachers/HomeTeachers";
 import HomeProjectInfo from "./HomeProjectInfo/HomeProjectInfo";
 import HomeForm from "./HomeForm/HomeForm";
 import Footer from "../../components/Footer/Footer";
+import axios from "axios";
+import { useState } from "react";
 
 export default function HomeLayaot() {
+  const [teachers, setTeachers] = useState([]);
+
+  useEffect(() => {
+    const fetchTeachers = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4200/api/teacher/all"
+        );
+        setTeachers(response.data);
+      } catch (error) {
+        console.error("Ошибка при получении данных:", error);
+      }
+    };
+
+    fetchTeachers();
+  }, []);
+
   return (
     <>
+      <div>
+        <h2>Список преподавателей</h2>
+        <ul>
+          {teachers.map((teacher) => (
+            <li key={teacher.id}>
+              {teacher.fullName} — {teacher.description}
+            </li>
+          ))}
+        </ul>
+      </div>
       <div className={styles.container}>
         <HomeTitle />
         <HomeDescription />
